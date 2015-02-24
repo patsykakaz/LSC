934c6e79aa82e2549c5d12d2d2f80ff52b00474a
patsykakaz <patsykakaz@iMac-de-patsykakaz.local>
2015-02-24 11:44:15 +0100

    ...

--- js/main.js
+++ js/main.js
@@ -16,3 +16,12 @@ $(document).ready(function(){
     });
 });
 
+$(window).resize(function(){
+    $('#layer-logo').css('width', $(window).width()).css('height',$(window).height());
+    if($(window).width()>$(window).height()){
+        $('#logo').css('height',$(window).height()/1.5).css('width',$(window).height()/1.5);
+    }else{
+        $('#logo').css('height',$(window).width()/1.5).css('width',$(window).width()/1.5);
+    }
+    $('#logo').css('margin-top',($(window).height()-$('#logo').outerHeight())/2+'px');
+});
