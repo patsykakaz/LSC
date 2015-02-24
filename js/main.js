651306fc74cf0ec9554e0df06ebcc0b918911edd
patsykakaz <patsykakaz@iMac-de-patsykakaz.local>
2015-02-24 11:24:11 +0100

    changement effet .logo

--- js/main.js
+++ js/main.js
@@ -4,18 +4,15 @@ $(document).ready(function(){
     if($(window).width()>$(window).height()){
         $('#logo').css('height',$(window).height()/1.5).css('width',$(window).height()/1.5);
     }else{
-        $('#logo').css('height',$(window).width()*1).css('width',$(window).width()*1);
+        $('#logo').css('height',$(window).width()/1.5).css('width',$(window).width()/1.5);
     }
     $('#logo').css('margin-top',($(window).height()-$('#logo').outerHeight())/2+'px');
 
     $(window).load(function(){
-        $('#logo').addClass('scale-test');
-        setTimeout(function() {
-            $('#logo').removeClass('logo-hide');
-        }, 100);
+        $('#logo').removeClass('logo-hide');
         setTimeout(function() {
             $('#logo').addClass('logo-rotate');
-        }, 5000);
+        }, 3000);
     });
 });
 
